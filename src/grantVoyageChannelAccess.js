976916af4fa70fcf12ae1da27217fc2770373dbd
Jim Medlock <jdmedlock@gmail.com>
2021-05-31 18:26:46 -0500

    Feature: Add authorizations for voice channels [WIP]

--- src/grantVoyageChannelAccess.js
+++ src/grantVoyageChannelAccess.js
@@ -3,6 +3,38 @@ import FileOps from './FileOps.js'
 import initializeProgressBars from './initializeProgressBars.js'
 
 const grantVoyageChannelAccess = async (environment, GUILD_ID, DISCORD_TOKEN, TEAMS, VALIDATE) => {
+  
+  const getChannel = (discordIntf, guild, categoryName, team) => {
+    let channel = discordIntf.isChannelCreated(guild, categoryName, team.team.name)
+    if (channel.length === 0) {
+      throw new Error(`This team channel (${ team.team.name }) hasn't been \
+      defined yet. Please create it before continuing.`)
+    }
+  }
+
+  const grantUserAccess = async (type, channel, team) => {
+    for (let userName of team.team.discord_names) {
+      const user = await guild.members.fetch({ query: `${ userName }`, limit: 1 })
+      if (VALIDATE) {
+        if (user.size === 0) {
+          console.log('Validation failed for user: ', userName)
+        }
+      } else {
+        await channel.updateOverwrite(user.first().user,
+          {
+            'VIEW_CHANNEL': true,
+            'SEND_MESSAGES': true,
+            'EMBED_LINKS': true,
+            'ATTACH_FILES': true,
+            'ADD_REACTIONS': true,
+            'MENTION_EVERYONE': true,
+            'MANAGE_MESSAGES': true,
+            'READ_MESSAGE_HISTORY': true
+          }
+        )
+      }
+    }
+  }
   const discordIntf = new Discord(environment)
   const rawTeams = FileOps.readFile(TEAMS)
   const teams = JSON.parse(rawTeams)
@@ -16,7 +48,7 @@ const grantVoyageChannelAccess = async (environment, GUILD_ID, DISCORD_TOKEN, TE
 
   try {
     client.on('ready', async () => {
-      // Authorize voyager access to the Voyage channels
+      // Authorize voyagers access to their Voyage channels
       const categoryName = discordIntf.generateCategoryName(teams)
       let category = discordIntf.isCategoryCreated(guild, categoryName)
       if (category.length === 0) {
@@ -28,30 +60,11 @@ const grantVoyageChannelAccess = async (environment, GUILD_ID, DISCORD_TOKEN, TE
       // Authorize teammember access to the team channels
       let teamNo = 0
       for (let team of teams.teams) {
-        let channel = discordIntf.isChannelCreated(guild, categoryName, team.team.name)
-        if (channel.length === 0) {
-          throw new Error(`This team channel (${ team.team.name }) hasn't been \
-          defined yet. Please create it before continuing.`)
-        }
-        // Authorize access to the channel
         if (team.team.discord_names.length > 0) {
-          for (let userName of team.team.discord_names) {
-            const user = await guild.members.fetch({ query: `${ userName }`, limit: 1 })
-            if (VALIDATE) {
-              if (user.size === 0) {
-                console.log('Validation failed for user: ', userName)
-              }
-            } else {
-              await channel[0].updateOverwrite(user.first().user,
-                {
-                  'VIEW_CHANNEL': true,
-                  'SEND_MESSAGES': true,
-                  'EMBED_LINKS': true,
-                  'ATTACH_FILES': true,
-                }
-              )
-            }
-          }
+          let textChannel = getChannel(discordIntf, guild, categoryName, team)
+          grantUserAccess('text', textChannel, team)
+          //let voiceChannel = getChannel(discordIntf, guild, categoryName, team.team.name.concat('av'))
+          //grantUserAccess('voice', voiceChannel, team)
         }
         progressBars[teamNo+1].increment(1)
         progressBars[ALL_TEAMS].increment(1) 
