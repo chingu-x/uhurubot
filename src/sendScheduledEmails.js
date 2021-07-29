593dfbcc33956edc2293d51df9b960e34414da11
jdmedlock <jdmedlock@gmail.com>
2021-07-29 14:15:45 -0500

    Feature: Add Airtable NotificationQueue table retrieval [WIP]

--- src/sendScheduledEmails.js
+++ src/sendScheduledEmails.js
@@ -5,17 +5,21 @@ const emailScheduledMessages = async (environment, SCHEDULE) => {
   const schedule = JSON.parse(rawSchedule)
   console.log('schedule: ', schedule)
 
-  // Scan Notification Status table to identify members where at least one
+  // Scan Notification Queue table to identify members where at least one
   // email remains to be sent and have not:
   // 1. Unsubscribed from emails
   // 2. Submitted a Solo Project Status
   // 3. Become an inactive member
+  
 
   // Using the current date, for each member calculate how many days have 
   // elapsed since their application was approved & which email should be sent
+  // based on the schedule
 
   // Process the list of emails to be sent. Each entry will be sent to
-  // UhuruBE for transmission
+  // UhuruBE for transmission and the Notifications table will be updated
+  // to record the event and the Notification Queue table will be update when
+  // the last email is sent to mark the notification as finished.
 }
 
 export default emailScheduledMessages
