668b35eb4d121c151681e036af24019a0bdbe80d
jdmedlock <jdmedlock@gmail.com>
2023-04-30 16:38:31 -0500

    Feature: Add team resource message

--- src/buildVoyageTeamConfig.js
+++ src/buildVoyageTeamConfig.js
@@ -33,30 +33,33 @@ const addVoyagerToTeam = (teams, currentTeamNo, voyagerCategory, voyager) => {
     currentTeamNo = voyager.team_no
     const teamName = voyager.tier.concat('-team-',voyager.team_no.padStart(2,0))
     teams.push({
+      team: {
         "category": voyagerCategory, 
         "name": teamName, 
         "tier": voyager.tier,
         "discord_names": [voyager.discord_name],
         "github_names": [voyager.github_name] ? [voyager.github_name] : '',
         "resource_msg": []
+      }
     })
     return voyager.team_no
   }
 
   const team = teams.slice(-1)[0]
-  const discordNames = team.discord_names
-  const githubNames = team.github_names
+  const discordNames = team.team.discord_names
+  const githubNames = team.team.github_names
   discordNames.push(voyager.discord_name)
   githubNames.push(voyager.github_name)
   return voyager.team_no
 }
 
 const addTeamResourcesToTeam = (team, teamNo, voyagers) => {
-  const findPOs = (voyager) => voyager.team_no === teamNo && voyager.role === 'Product Owner'
-  const findUIUXs = (voyager) => voyager.team_no === teamNo && voyager.role === 'UI/UX'
-  const findWebdevs = (voyager) => voyager.team_no === teamNo && voyager.role === 'Developer'
-  const findDataScientists = (voyager) => voyager.team_no === teamNo && voyager.role === 'Data Scientist'
-  const findVoyageGuides = (voyager) => voyager.team_no === teamNo && voyager.role === 'Voyage Guide'
+  const teamNumber = teamNo.padStart(2,0)
+  const findPOs = (voyager) => voyager.team_no.padStart(2,0) === teamNumber && voyager.role === 'Product Owner'
+  const findUIUXs = (voyager) => voyager.team_no.padStart(2,0) === teamNumber && voyager.role === 'UI/UX'
+  const findWebdevs = (voyager) => voyager.team_no.padStart(2,0) === teamNumber && voyager.role === 'Developer'
+  const findDataScientists = (voyager) => voyager.team_no.padStart(2,0) === teamNumber && voyager.role === 'Data Scientist'
+  const findVoyageGuides = (voyager) => voyager.team_no.padStart(2,0) === teamNumber && voyager.role === 'Voyage Guide'
 
   const productOwners = voyagers.filter(findPOs) ? voyagers.filter(findPOs)
     .map(voyager => '@'.concat(voyager.discord_name)) : 'None'
@@ -68,11 +71,11 @@ const addTeamResourcesToTeam = (team, teamNo, voyagers) => {
     .map(voyager => '@'.concat(voyager.discord_name)) : 'None'
   const voyageGuides = voyagers.filter(findVoyageGuides) ? voyagers.filter(findVoyageGuides)
     .map(voyager => '@'.concat(voyager.discord_name)) : 'None'
-  const githubRepoURL = `https://github.com/chingu-voyages/v44-tier2-team-${ teamNo.padStart(2,0) }`
+  const githubRepoURL = `https://github.com/chingu-voyages/v44-tier-team-${ teamNo.padStart(2,0) }`
   const gdrivePlaceholderURL = 'https://drive.google.com/drive/folders/'
 
   const resourceMsg = [
-    `**__${ voyagers[0].voyage.concat('-',voyagers[0].tier,'-team-',voyagers[0].team_no) } team:__**\n`,
+    `**__${ voyagers[0].voyage.concat('-',team.team.name) } team:__**\n`,
     `- Product Owners: ${ productOwners.toString() }\n`,
     `- UI/UX Designer: ${ uiuxDesigners.toString() }\n`,
     `- Web Developers: ${ webDevelopers.toString() }\n`,
@@ -83,7 +86,7 @@ const addTeamResourcesToTeam = (team, teamNo, voyagers) => {
     `- Google Drive: ${ gdrivePlaceholderURL} \n`,
   ]
 
-  team.resource_msg = resourceMsg
+  team.team.resource_msg = resourceMsg
 }
 
 const buildVoyageTeamConfig = async (environment, VOYAGE) => {
@@ -134,8 +137,7 @@ const buildVoyageTeamConfig = async (environment, VOYAGE) => {
   }
 
   config.teams.forEach(team => {
-    addTeamResourcesToTeam(team, team.name.slice(-2), voyagers)
-    console.log('team.resource_msg: ', team.resource_msg)
+    addTeamResourcesToTeam(team, team.team.name.slice(-2), voyagers)
   })  
 
   try { 
